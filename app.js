02c071de52038ef29399d171d9e2bca8d827e417
Karen Nason <knason4@gmail.com>
2022-10-15 22:55:40 -0500

    add files

---
+++ app.js
@@ -0,0 +1,44 @@
+// Write a function called countdown that accepts a number as a parameter and 
+// every 1000 milliseconds decrements the value and console.logs it. 
+// Once the value is 0 it should log “DONE!” and stop.
+
+
+
+function countDown(number) {
+    let n = number;
+
+    setInterval(function () {
+        n--;
+        if(n == 0) {
+            console.log("DONE!");
+            clearInterval(1);
+        }
+        while(n >= 1) {
+            return console.log(n);
+        }  
+    }, 1000);
+}
+
+
+/* 
+Write a function called randomGame that selects a random number between 0 
+and 1 every 1000 milliseconds and each time that a random number is picked, 
+add 1 to a counter. If the number is greater than .75, stop the timer and 
+console.log the number of tries it took before we found a number greater than .75.
+*/
+
+function randomGame () {
+    let counter = 0;
+    let randomNumber = 0;
+    setInterval(function () {
+        randomNumber = Math.random();
+        console.log(randomNumber);
+        counter++;
+        if(randomNumber > 0.75) {
+            console.log(counter);
+            clearInterval(1);
+        }
+    }, 1000);
+
+}  
+    
